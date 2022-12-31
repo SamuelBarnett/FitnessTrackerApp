0eeb706a5ccf8542fd49dc84329d412052d5b99e
Samuel Barnett <sambiam888@gmail.com>
2022-12-30 19:16:38 -0500

    goals to jsx

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import Goals from './pages/goals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Goals/>
   </React.StrictMode>
 );
 
